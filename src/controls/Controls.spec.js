d6a40f40be59c97efed7c0448de9b2d8de97c049
Aaron <aarongillies88@gmail.com>
2019-12-16 19:48:26 -0500

    Tests written and passing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls';
+
+test('Display renders properly', () => {
+    render(<Controls />);
+});
+
+test('cannot be closed or opened if it is locked', () => {
+    const toggle = jest.fn();
+    const { getByText } = render(<Controls
+        locked closed toggle={toggle}/>);
+    const openGate = getByText(/open gate/i);
+    fireEvent.click(openGate);
+    expect(toggle).not.toHaveBeenCalled();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Display renders properly', () => {
+    render(<Dashboard />);
+});
+
