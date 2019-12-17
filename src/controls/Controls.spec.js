32232d46a0b698d2692c358606200e73e5b1fca0
Aaron <aarongillies88@gmail.com>
2019-12-16 20:17:25 -0500

    Added a few more tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -24,3 +24,24 @@ test('can be opened or closed if it is unlocked', () => {
     expect(toggle).not.toHaveBeenCalled();
 });
 
+test('the closed toggle button is disabled if the gate is locked', () => {
+    const toggleClosed = jest.fn();
+    const { getByTestId } = render(<Controls 
+        locked closed toggleClosed={toggleClosed} />);
+    const closedIsDisabled = getByTestId('toggle-closed');
+    fireEvent.click(closedIsDisabled);
+    expect(toggleClosed).not.toHaveBeenCalled();
+});
+
+test('the locked toggle button is disabled if the gate is open', () => {
+    const toggleLocked = jest.fn();
+    const { getByTestId } = render(<Controls 
+        locked closed toggleLocked={toggleLocked}/>);
+    const lockedIsDisabled = getByTestId('toggle-locked');
+    fireEvent.click(lockedIsDisabled);
+    expect(toggleLocked).toHaveBeenCalled();
+});
+
+
+
+
